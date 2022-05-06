c730b02e6245afbc14c9e3905bcad10387f61f68
kuhjuice <kuhjuice>
2022-05-06 16:49:44 +0200

    functionFinish

--- API/api.py
+++ API/api.py
@@ -1,24 +1,29 @@
 from geopatterns import GeoPattern
-
 import random, string
-
 from PIL import Image
-
 import cairosvg
 
 def randomword(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
 
-string_to_generate = randomword(10)
-pattern = GeoPattern(string_to_generate, generator='xes')
-print(pattern.svg_string)
-textfile = open("patterntest_%s.svg"%string_to_generate, "w")
-a = textfile.write(pattern.svg_string)
-textfile.close()
+def generatePattern(sting):
+    pattern = GeoPattern(string_to_generate, generator='xes')
+    cairosvg.svg2png(bytestring=pattern.svg_string, write_to="images/patterntest_%s.png"%string_to_generate)
+    fp = open("images/patterntest_%s.png"%string_to_generate, "rb")
+    pil_image = Image.open(fp, mode='r', formats=None)
+    return pil_image
 
-cairosvg.svg2png(bytestring=pattern.svg_string, write_to="patterntest_%s.png"%string_to_generate)
+if __name__ == "__main__":
+    string_to_generate = randomword(10)
+    pattern = GeoPattern(string_to_generate, generator='xes')
+    print(pattern.svg_string)
+    textfile = open("patterntest_%s.svg"%string_to_generate, "w")
+    a = textfile.write(pattern.svg_string)
+    textfile.close()
 
-fp = open("patterntest_%s.png"%string_to_generate, "rb")
-pil_image = Image.open(fp, mode='r', formats=None)
-pil_image.show()
+    cairosvg.svg2png(bytestring=pattern.svg_string, write_to="patterntest_%s.png"%string_to_generate)
+
+    fp = open("patterntest_%s.png"%string_to_generate, "rb")
+    pil_image = Image.open(fp, mode='r', formats=None)
+    pil_image.show()
