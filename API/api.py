91c4c267500f6439fcfdd422ddf95a0705f1a8a1
kuhjuice <kuhjuice>
2022-05-06 16:20:29 +0200

    sample images

--- API/api.py
+++ API/api.py
@@ -1,6 +1,14 @@
 from geopatterns import GeoPattern
-pattern = GeoPattern('A string for your consideration.', generator='xes')
+
+import random, string
+
+def randomword(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+string_to_generate = randomword(10)
+pattern = GeoPattern(string_to_generate, generator='xes')
 print(pattern.svg_string)
-textfile = open("patterntest.svg", "w")
+textfile = open("patterntest_%s .svg"%string_to_generate, "w")
 a = textfile.write(pattern.svg_string)
 textfile.close()
