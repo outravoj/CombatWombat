7961fc6c892f968b11724f07ee6e99c79f07f007
Outris <79868016+Outris@users.noreply.github.com>
2022-05-11 21:55:13 +0300

    I added the prompting webpage styled in the same manner as the resultng page.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('form.html')
+    return render_template('playground.html')
     #imagePathList = ['Programming.JPG']
     #return render_template('home.html', imageList=imagePathList)
 
