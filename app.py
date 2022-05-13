d749f8f37b5273e272dd9d3a1bf6361671d0a393
Outris <79868016+Outris@users.noreply.github.com>
2022-05-13 14:38:44 +0300

    Cora's designed submitting page.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('playground.html')
+    return render_template('grid_pplayground.html')
     #imagePathList = ['Programming.JPG']
     #return render_template('home.html', imageList=imagePathList)
 
